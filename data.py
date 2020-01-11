17c274d8353637f39b0920278f61e96b9397347e
Gabriel Cordovado <gabe.cordovado@yahoo.ca>
2020-01-11 12:25:51 -0500

    primary files

---
+++ data.py
@@ -0,0 +1,2 @@
+def GenericData():
+	pass
---
+++ main.py
@@ -0,0 +1,13 @@
+########################################################################
+# Impoorts Required for Flask Server Functionality
+########################################################################
+from flask import Flask, render_template, url_for, abort, redirect
+
+########################################################################
+# Primative and non-Primitve user-data imports for text/database
+########################################################################
+from data import GenericData
+
+########################################################################
+# Server Settup / Socket Address Initilization and Referects
+########################################################################
