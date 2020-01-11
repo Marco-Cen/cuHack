098e2e9299e825ea0e2847da3b762692935ddbe2
Gabriel Cordovado <gabe.cordovado@yahoo.ca>
2020-01-11 13:07:44 -0500

    added required files and directories

--- main.py
+++ main.py
@@ -11,3 +11,24 @@ from data import GenericData
 ########################################################################
 # Server Settup / Socket Address Initilization and Referects
 ########################################################################
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('report.html', title='GeoMap | Report an Issue')
+
+@app.route('/report')
+def home():
+	return redirect(url_for('/'))
+
+@app.route('/display')
+def about():
+	return render_template('display.html', title='GeoMap | Current Reports Around You')
+
+@app.errorhandler(404)
+def handle(error):
+	return render_template('handler.html', title='404 Error')
+	
+if __name__ == "__main__":
+	app.run(debug=True)
